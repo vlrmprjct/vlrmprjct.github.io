d98fcaba9fe0a2a5087e33ff416a3f3c1080e182
Thomas M <mail@lokalnetz.com>
2024-07-01 14:05:18 +0200

    reformat about view, add fa icons

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,57 +1,57 @@
 import React from 'react';
-// import GitHubIcon from '@material-ui/icons/GitHub'
-// import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { about } from '../../portfolio';
 
 import './About.scss'
 
 export const About = () => {
-  const { name, role, description, resume, social } = about
+    const { name, role, description, resume, social } = about
 
-  return (
-    <div className='about center'>
-      {name && (
-        <h1>
-          Hi, I am <span className='about__name'>{name}.</span>
-        </h1>
-      )}
-
-      {role && <h2 className='about__role'>{role}.</h2>}
-      <p className='about__desc'>{description && description}</p>
-
-      <div className='about__contact center'>
-        {resume && (
-          <a href={resume}>
-            <span className='btn btn--outline'>
-              Resume
-            </span>
-          </a>
-        )}
-
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                G{/* <GitHubIcon /> */}
-              </a>
+    return (
+        <div className='about center'>
+            {name && (
+                <h1>
+                    Hi, I am <span className='about__name'>{name}.</span>
+                </h1>
             )}
 
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                LI{/* <LinkedInIcon /> */}
-              </a>
-            )}
-          </>
-        )}
-      </div>
-    </div>
-  )
+            {role && <h2 className='about__role'>{role}.</h2>}
+            <p className='about__desc'>{description && description}</p>
+
+            <div className='about__contact center'>
+                {resume && (
+                    <a href={resume}>
+                        <span className='btn btn--outline'>
+                            Resume
+                        </span>
+                    </a>
+                )}
+
+                {social && (
+                    <>
+                        {social.github && (
+                            <a
+                                href={social.github}
+                                aria-label='github'
+                                className='link link--icon'
+                            >
+                                <FontAwesomeIcon icon={faGithub} size="2x" />
+                            </a>
+                        )}
+
+                        {social.linkedin && (
+                            <a
+                                href={social.linkedin}
+                                aria-label='linkedin'
+                                className='link link--icon'
+                            >
+                                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                            </a>
+                        )}
+                    </>
+                )}
+            </div>
+        </div>
+    )
 }
